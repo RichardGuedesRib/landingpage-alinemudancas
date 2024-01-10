6ff3ac4e1e8315650d55600ca2661d84bbbc1b91
Richard Guedes <richard.silva46@fatec.sp.gov.br>
2024-01-10 16:51:07 -0300

    Entrega da versao primaria - Acerto de Imagem e Atualização do numero de telefone do cliente

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const btnEntreEmContatoMenu = document.getElementById(
+    "btnEntreEmContatoMenu"
+  );
+  const modal = document.getElementById("modal");
+  const fechar = document.getElementById("fechar");
+  const enviar = document.getElementById("enviar");
+  const origemInput = document.getElementById("origem");
+  const destinoInput = document.getElementById("destino");
+  const mensagemTextarea = document.getElementById("mensagem");
+  const linkWhatsAppFooter = document.getElementById("linkWhatsAppFooter");
+  const iconWhatsApp = document.getElementById("iconWhatsApp");
+
+  btnEntreEmContatoMenu.addEventListener("click", function () {
+    modal.style.display = "block";
+  });
+
+  // iconWhatsApp.addEventListener("click", function () {
+  //   modal.style.display = "block";
+  // });
+
+  linkWhatsAppFooter.addEventListener("click", function () {
+    modal.style.display = "block";
+  });
+
+  fechar.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+  enviar.addEventListener("click", function () {
+    const origem = origemInput.value;
+    const destino = destinoInput.value;
+    const mensagem = mensagemTextarea.value;
+    const numeroTelefone = "+5511953038151";
+    const mensagemWhatsApp = `Olá! Vim Através do Site!%0AGostaria de um orçamento!%0AOrigem: ${origem}%0ADestino: ${destino}%0ADescrição dos Móveis: ${mensagem}`;
+
+    window.open(`https://wa.me/${numeroTelefone}?text=${mensagemWhatsApp}`);
+    modal.style.display = "none";
+  });
+});
